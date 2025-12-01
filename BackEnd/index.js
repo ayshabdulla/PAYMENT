0cd22282f30eccc34ccfd33ccc896ba864e18bd6
ayshabdulla <ayshfanay@gmail.com>
2025-12-02 00:29:18 +0530

    Fix upload path

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -13,7 +13,10 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use("/uploads", express.static("uploads"));
+const path = require("path");
+
+app.use("/uploads", express.static(path.join(__dirname, "Uploads")));
+
 
 //ROUTEs+
 app.use("/api/products", productRoutes);
